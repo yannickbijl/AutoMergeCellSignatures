3975e5de019c6ac2849960e4260ba381c389b5fe
Yannick Bijl <yannick.bijl@gmail.com>
2019-10-04 22:45:35 +0200

    Set names to readable distance format.

--- AutoMergeCellSignatures.R
+++ AutoMergeCellSignatures.R
@@ -4,3 +4,4 @@ cell_signatures <- read.csv(filename_cell_signatures, row.names=1, sep=";")
 
 distance_matrix <- abs(dist(t(cell_signatures)))
 cell_signature_distances <- data.frame(t(combn(rownames(t(cell_signatures)),2)), as.numeric(distance_matrix))
+names(cell_subset_distances) <- c("subset1", "subset2", "distance")
