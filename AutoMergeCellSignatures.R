459da3d86b34b68dd7497cbaf421bce9f02572a3
Yannick Bijl <yannick.bijl@gmail.com>
2019-10-04 22:41:49 +0200

    Make distance matrix, and transform it into readable format.

--- AutoMergeCellSignatures.R
+++ AutoMergeCellSignatures.R
@@ -1,3 +1,6 @@
 filename_cell_signatures <- "D:\\USERYB\\Downloads\\test.csv"
 
 cell_signatures <- read.csv(filename_cell_signatures, row.names=1, sep=";")
+
+distance_matrix <- abs(dist(t(cell_signatures)))
+cell_signature_distances <- data.frame(t(combn(rownames(t(cell_signatures)),2)), as.numeric(distance_matrix))
