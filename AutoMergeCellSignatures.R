660e11a4879bf20e8f9559fa337ce74822e78b3e
Yannick Bijl <yannick.bijl@gmail.com>
2019-10-06 20:07:11 +0200

    Working script.
    - Save the resulting signatures to a specified file in csv.
    - TODO: place comments.

--- AutoMergeCellSignatures.R
+++ AutoMergeCellSignatures.R
@@ -1,6 +1,7 @@
 
 # Parameters.
 filename_cell_signatures <- "D:\\USERYB\\Downloads\\test.csv"
+filename_out <- "D:\\USERYB\\Downloads\\test_out.csv"
 max_distance <- 2
 max_marker_difference <- 1
 
@@ -8,16 +9,19 @@ max_marker_difference <- 1
 cell_signatures <- read.csv(filename_cell_signatures, row.names=1, sep=";")
 
 # Initial distance matrix.
-distance_matrix <- abs(dist(t(cell_signatures)))
-cell_signature_distances <- data.frame(t(combn(rownames(t(cell_signatures)),2)), as.numeric(distance_matrix))
-names(cell_signature_distances) <- c("subset1", "subset2", "distance")
+if (ncol(cell_signatures) == 1) {
+    write.csv(cell_signatures, file=filename_out)
+    quit()
+} else {
+    distance_matrix <- abs(dist(t(cell_signatures)))
+    signature_distances <- data.frame(t(combn(rownames(t(cell_signatures)),2)), as.numeric(distance_matrix))
+    names(signature_distances) <- c("subset1", "subset2", "distance")
+}
 
-while (min(cell_signature_distances$distance) < max_distance) {
-    index <- which(cell_signature_distances$distance == min(cell_signature_distances$distance))
-
-    name_1 <- toString(cell_signature_distances[index[1], "subset1"]) # subset1 column
-    name_2 <- toString(cell_signature_distances[index[1], "subset2"]) # subset2
-
+while (min(signature_distances$distance) < max_distance) {
+    index <- which(signature_distances$distance == min(signature_distances$distance))
+    name_1 <- toString(signature_distances[index[1], "subset1"]) # subset1 column
+    name_2 <- toString(signature_distances[index[1], "subset2"]) # subset2
     subset_1 <- cell_signatures[,name_1]
     subset_2 <- cell_signatures[,name_2]
     marker_difference = abs(subset_1 - subset_2)
@@ -26,7 +30,18 @@ while (min(cell_signature_distances$distance) < max_distance) {
         new_cell_signature<- paste(name_1, name_2, sep="_")
         cell_signatures[[new_cell_signature]] <- rowMeans(cell_signatures[,c(name_1, name_2)])
         cell_signatures <- cell_signatures[,!(names(cell_signatures) %in% c(name_1, name_2))]
+        cell_signatures <- as.data.frame(cell_signatures)
+        if (ncol(cell_signatures) == 1) {
+            write.csv(cell_signatures, file=filename_out)
+            quit()
+        } else {
+            distance_matrix <- abs(dist(t(cell_signatures)))
+            signature_distances <- data.frame(t(combn(rownames(t(cell_signatures)),2)), as.numeric(distance_matrix))
+            names(signature_distances) <- c("subset1", "subset2", "distance")
+        }
+    } else {
+       signature_distances[index,3] <- Inf # distance column
     }
-    cell_signature_distances[index,3] <- Inf # distance column
 }
-print(cell_signatures)
+write.csv(cell_signatures, file=filename_out)
+quit()
